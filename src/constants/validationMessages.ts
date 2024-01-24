113c9af92895bffbe99ebb3470f27e6715720da0
sunnyDev93 <sunny.dev199093@gmail.com>
2024-01-24 23:06:27 +0100

    fix

--- src/constants/validationMessages.ts
+++ src/constants/validationMessages.ts
@@ -1,10 +1,10 @@
 export const validationMessages = {
-  email: "Email is Invalid",
-  password: {
-    min: "Password should be at least 6 Characters",
-    max: "Password is too long",
-  },
-  required: (field: string) => {
-    return `${field} field is required`
-  }
+    email: "Email is Invalid",
+    password: {
+        min: "Password should be at least 6 Characters",
+        max: "Password is too long"
+    },
+    required: (field : string) => {
+        return `${field} field is required`
+    }
 };
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,24 +1,20 @@
-import { Router } from "express";
-import { Request, Response } from "express";
+import {Router} from "express";
+import {Request, Response} from "express";
 
-import { verifyToken } from "../middlewares/auth.middleware";
+import {verifyToken} from "../middlewares/auth.middleware";
 import authController from "../controllers/auth.controller";
-import { validatePayload } from "../middlewares/payloadvalidator.middleware";
-import { signinSchema } from "../validators/signin.schema";
-import { verifyItemByIdSchema } from "../validators/verifyuserbyid.schema";
+import {validatePayload} from "../middlewares/payloadvalidator.middleware";
+import {signinSchema} from "../validators/signin.schema";
+import {verifyItemByIdSchema} from "../validators/verifyuserbyid.schema";
 
 const authRoute: Router = Router();
 
 authRoute.post("/login", validatePayload(signinSchema), authController.login);
 authRoute.post("/signup", validatePayload(signinSchema), authController.signup);
-authRoute.post(
-  "/verifyuserbyid/:id",
-  validatePayload(verifyItemByIdSchema),
-  authController.verifyUserById
-);
+authRoute.post("/verifyuserbyid/:id", validatePayload(verifyItemByIdSchema), authController.verifyUserById);
 authRoute.post("/forgot-password", authController.forgotPassword);
-authRoute.get("/me", verifyToken, async (req: Request, res: Response) => {
-  await authController.fetchMe(req, res);
+authRoute.get("/me", verifyToken, async (req : Request, res : Response) => {
+    await authController.fetchMe(req, res);
 });
 
 export default authRoute;
